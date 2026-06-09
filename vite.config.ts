8fbf003f9866b9c76af1002aaead341e59c447b9
OIeksandr Kuznietsov <57156183+Disdainer0@users.noreply.github.com>
2026-06-09 01:36:06 +0000

    3333

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  plugins: [],
   vite: {
     plugins: [],
   },
   tanstackStart: {
-    server: { entry: "server" },
+    server: {
+      entry: "server",
+    },
   },
 });
